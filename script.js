80f565f4fac176580555c04acfe9be6494c15ccf
Gabriel Politano <gabriel.cpolitano@gmail.com>
2024-04-16 08:53:45 -0300

    1.0

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function press(num) {
     currentInput += num;
     document.getElementById('display').value = currentInput;
 }
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput).toString();
+        document.getElementById('display').value = currentInput;
+    } catch (e) {
+        document.getElementById('display').value = 'Erro';
+        currentInput = '';
+    }
+}
+
+function clearDisplay() {
+    currentInput = '';
+    document.getElementById('display').value = '';
+}
